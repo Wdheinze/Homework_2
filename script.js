e7377a28b7e3eba45b41687a8229fb62e7fc5898
Wdheinze <77468808+Wdheinze@users.noreply.github.com>
2021-01-22 21:56:40 -0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("Facebook");
+//Facebook is a social media site
+console.log("Gmail");
+//Gmail is what I use for my Email
+console.log("Toyhou.se");
+//Toyhouse is where I keep all of my original characters
+console.log("Cyberpunk 2077");
+//Cyberpunk is an amazing shooter-story game
+console.log("The Trine Seies");
+//The Trine series is an amazing 2-d side scroller
+console.log("Genshin Impact");
+//Genshin Impact is a free to play game I love
+console.log("Midori")
+//My good friend who is an anime artist
+console.log("Banksy")
+//enough said. he is amazing
+console.log("Laovaan")
+//He is another anime artist who draws very handsome men
